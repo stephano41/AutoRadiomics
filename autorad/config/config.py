f4e3e0fef02d68c3038cdc42300c3a5ba24bbaf0
stephano41 <78075921+stephano41@users.noreply.github.com>
2023-09-23 22:34:06 +0930

    Update config.py to not have logging by default

--- autorad/config/config.py
+++ autorad/config/config.py
@@ -59,49 +59,51 @@ SEED = 123
 PYRADIOMICS_TMP_DIR = tempfile.mkdtemp()
 IS_DEMO = False
 
-# Logging
-LOGS_DIR = Path(RESULT_DIR, "logs")
-LOGS_DIR.mkdir(parents=True, exist_ok=True)
-
-logging_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "minimal": {"format": "%(message)s"},
-        "detailed": {
-            "format": "%(levelname)s %(asctime)s [%(name)s:%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+ENABLE_AUTORAD_LOGGING = os.getenv("ENABLE_AUTORAD_LOGGING")
+if ENABLE_AUTORAD_LOGGING:
+    # Logging
+    LOGS_DIR = Path(RESULT_DIR, "logs")
+    LOGS_DIR.mkdir(parents=True, exist_ok=True)
+    
+    logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "minimal": {"format": "%(message)s"},
+            "detailed": {
+                "format": "%(levelname)s %(asctime)s [%(name)s:%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+            },
         },
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "stream": sys.stdout,
-            "formatter": "minimal",
-            "level": logging.DEBUG,
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": sys.stdout,
+                "formatter": "minimal",
+                "level": logging.DEBUG,
+            },
+            "info": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": Path(LOGS_DIR, "info.log"),
+                "maxBytes": 10485760,  # 1 MB
+                "backupCount": 10,
+                "formatter": "detailed",
+                "level": logging.INFO,
+            },
+            "error": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": Path(LOGS_DIR, "error.log"),
+                "maxBytes": 10485760,  # 1 MB
+                "backupCount": 10,
+                "formatter": "detailed",
+                "level": logging.ERROR,
+            },
         },
-        "info": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": Path(LOGS_DIR, "info.log"),
-            "maxBytes": 10485760,  # 1 MB
-            "backupCount": 10,
-            "formatter": "detailed",
+        "root": {
+            "handlers": ["console", "info", "error"],
             "level": logging.INFO,
+            "propagate": True,
         },
-        "error": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": Path(LOGS_DIR, "error.log"),
-            "maxBytes": 10485760,  # 1 MB
-            "backupCount": 10,
-            "formatter": "detailed",
-            "level": logging.ERROR,
-        },
-    },
-    "root": {
-        "handlers": ["console", "info", "error"],
-        "level": logging.INFO,
-        "propagate": True,
-    },
-}
+    }
 
 logging.config.dictConfig(logging_config)
 logger = logging.getLogger()
