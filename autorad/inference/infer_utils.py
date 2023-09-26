f2be35253371b70042394c43f8809ce739973571
stephano41 <78075921+stephano41@users.noreply.github.com>
2023-09-26 21:37:35 +0930

    Update infer_utils.py get latest run

--- autorad/inference/infer_utils.py
+++ autorad/inference/infer_utils.py
@@ -79,3 +79,22 @@ def plot_shap_waterfall(explainer, X_preprocessed, max_display=10):
         shap_values[0], max_display=max_display, show=True
     )
     return shap_fig
+
+def get_last_run_from_experiment_name(experiment_name="model_training"):
+    experiment_id = mlflow_utils.get_experiment_id_from_name(experiment_name)
+    all_runs = mlflow.search_runs(experiment_ids=experiment_id)
+    try:
+        last_run = all_runs.iloc[0]
+    except IndexError:
+        raise IndexError(
+            "No trained models found. Please run the training first."
+        )
+
+    return last_run
+
+
+def get_artifacts_from_last_run(experiment_name="model_training"):
+    last_run = get_last_run_from_experiment_name(experiment_name)
+    artifacts = load_pipeline_artifacts(last_run)
+
+    return artifacts
