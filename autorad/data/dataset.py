ae57b90de1597ad839ba4f75bf79472378033413
stephano41 <78075921+stephano41@users.noreply.github.com>
2024-05-02 15:39:45 +0930

    add leave one out

--- autorad/data/dataset.py
+++ autorad/data/dataset.py
@@ -263,6 +263,8 @@ class FeatureDataset:
             splits = self.full_split_no_test(split_on, test_size, *args, **kwargs)
         elif method == "repeated_stratified_kfold_no_test":
             splits = self.repeated_stratified_kfold_no_test(split_on, test_size, *args, **kwargs)
+        elif method =='leave_one_out_no_test':
+            splits = self.leave_one_out_no_test(split_on, *args, **kwargs)
         else:
             raise ValueError(f"Method {method} is not supported.")
 
@@ -440,6 +442,29 @@ class FeatureDataset:
         results = {
             "split_on": split_on,
             "split_type": f"{n_repeats} repeated stratified {n_splits} fold cross validation on training set only",
+            "test": [],  # add something to test to stop code from breaking, not intended to be used
+            "train": splits
+        }
+
+        return results
+
+    def leave_one_out_no_test(self, split_on, test_size):
+        patient_df = self.df[[split_on, self.target]].drop_duplicates()
+        if not patient_df[split_on].is_unique:
+            raise ValueError(
+                f"Selected column {split_on} has varying labels for the same ID!"
+            )
+        ids = patient_df[split_on].tolist()
+        labels = patient_df[self.target].tolist()
+        splits = splitting.split_cross_validation(
+            ids_train=ids,
+            y_train=labels,
+            cv_type="leave_one_out",
+        )
+
+        results = {
+            "split_on": split_on,
+            "split_type": f"leave one out on training set only",
             "test": [],  # add something to test to stop code from breaking, not intended to be used
             "train": splits
         }
